c49ded7447951bca442cbf1578de89b4161eb7ad
Ashish Kumar <ashishcool123456789@gmail.com>
2024-06-11 11:04:10 +0530

    final

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 export const Todo = ({ task, toggleComplete, editTodo, deleteTodo }) => {
-    return (
-        <div className={`flex justify-between m-10 text-xl border rounded-lg px-5 py-2.5 bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 ${task.completed ? 'line-through' : ''}`}>
-            <p onClick={() => toggleComplete(task.id)}>{task.task}</p>
-            <div className='cursor-pointer'>
-                <FontAwesomeIcon icon={faPenToSquare} className='mr-2'
-                    onClick={() => editTodo(task.id)} />
-                <FontAwesomeIcon icon={faTrash} onClick={() =>
-                    deleteTodo(task.id)
-                } />
-            </div>
-        </div>
-    );
+  const [isCompleted, setIsCompleted] = useState(task.completed); 
+
+  const handleClick = () => {
+    toggleComplete(task.id);
+    setIsCompleted(!isCompleted); 
+  };
+
+  return (
+    <div
+      className={`flex justify-between m-10 text-xl border rounded-lg px-5 py-2.5 bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 ${isCompleted ? 'line-through' : ''}`}
+    >
+      <p onClick={handleClick} className={`text-xl cursor-pointer ${isCompleted ? 'text-red-500' : 'text-white'}`}>
+        {task.task}
+      </p>
+      <div className='cursor-pointer'>
+        <FontAwesomeIcon icon={faPenToSquare} className='mr-2' onClick={() => editTodo(task.id)} />
+        <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)} />
+      </div>
+    </div>
+  );
 };
